0967b4461022f3533236e751cc32c468d303fd83
seb0xff <contact.git@seb0xff.dev>
2024-12-22 23:32:50 +0100

    Add titlebar fixer

---
+++ custom_audio_broadcaster_client/tweak_titlebar.py
@@ -0,0 +1,24 @@
+import gi
+from logger import logger
+
+gi.require_version('Gdk', '4.0')
+
+from gi.repository import Gtk, Gdk  # type: ignore
+
+
+def tweak_titlebar(window: Gtk.Window):
+  # ensure it's macos
+  from sys import platform
+  if platform != 'darwin':
+    return
+
+  try:
+    gi.require_version('Gnt', '0.1')
+    from gi.repository import Gnt  # type: ignore
+    tweaker = Gnt.MacosWindow(window=window)
+    color = Gdk.RGBA()
+    color.parse('rgb(18,19,29)')
+    tweaker.set_titlebar_color(color)
+
+  except Exception as e:
+    logger.info(f'Cannot tweak titlebar, falling back to defaults: {e}')
