9121897aee275f724fa6b520223a383bd59c75df
seb0xff <contact.git@seb0xff.dev>
2024-12-22 23:32:50 +0100

    Add '&' char handling

--- custom_audio_broadcaster_client/response_preview.py
+++ custom_audio_broadcaster_client/response_preview.py
@@ -7,16 +7,15 @@ from gi.repository import Gtk  # type: ignore
 
 json_keys_regex = r'(\".*?\")(?=:)'
 json_string_values_regex = r'(?<=:\s)(\".*?\")'
-json_non_string_values_regex = r'(?<=:\s)([0-9]+|null)'
+json_non_string_values_regex = r'(?<=:\s)([0-9]+|null|true|false)'
 
 
 class ResponsePreview(Gtk.ScrolledWindow):
 
   def __init__(self, json_str: str):
     super().__init__(vexpand=True)
-    self.text_view = Gtk.TextView(editable=False,
-                                  cursor_visible=False,
-                                  wrap_mode=Gtk.WrapMode.WORD_CHAR)
+    self.text_view = Gtk.TextView(editable=False, cursor_visible=False)
+    self.add_css_class('response-preview')
     self.set_child(self.text_view)
     self.set_str(json_str)
 
@@ -26,12 +25,13 @@ class ResponsePreview(Gtk.ScrolledWindow):
     if not json_str or buffer.get_text(
         buffer.get_start_iter(), buffer.get_end_iter(), True) == json_str:
       return
-    json_str = re.sub(json_keys_regex,
-                      r'<b><span foreground="yellow">\1</span></b>', json_str)
+    json_str = re.sub(r'&', '&amp;', json_str)
+    json_str = re.sub(json_keys_regex, r'<span foreground="#8aadf4">\1</span>',
+                      json_str)
     json_str = re.sub(json_string_values_regex,
-                      r'<span foreground="green">\1</span>', json_str)
+                      r'<span foreground="#a6da95">\1</span>', json_str)
     json_str = re.sub(json_non_string_values_regex,
-                      r'<span foreground="blue">\1</span>', json_str)
+                      r'<span foreground="#f5a97f">\1</span>', json_str)
     buffer = self.text_view.get_buffer()
     self.buffer = buffer
     self.buffer.set_text('')
